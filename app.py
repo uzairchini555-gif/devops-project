5ff113f5177bb993371010408d26222759f27822
Uzair Munir <uzairchini555-gmail.com>
2026-04-08 00:38:59 +0500

    updated main text

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-	return"""Q. What is linux?
+	return """Q. What is linux?<br>
 A. A Linux is an open-source OS (operating system) widely used in servers and cloud environment."""
 
 app.run(host="0.0.0.0", port=5000)
