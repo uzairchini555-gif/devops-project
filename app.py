b0e17b789bdc5188cdb5c46344a54b21701a63ba
Uzair Munir <uzairchini555-gif>
2026-04-13 23:23:01 +0500

    Added CI/CD Pipeline

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ app = Flask(__name__)
 def home():
 	return """Q. What is linux?<br>
 A. A Linux is an open-source OS (operating system) widely used in servers and cloud environment."""
-
-app.run(host="0.0.0.0", port=5000)
  
 @app.route("/health")
-daf health():
+def health():
 	return "OK", 200
+@app.route("/ready")
+def ready():
+	return jsonify({
+		"status": "ready"}), 200
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=5000)
+
