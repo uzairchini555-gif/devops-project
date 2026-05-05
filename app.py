e2350790563a415c2fcbdd66bd6ca354d2eb91b9
Uzair Munir <uzairchini555-gif>
2026-05-05 19:46:38 +0500

    Add CI/CD pipeline

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ def home():
 	return """This is Uzair, DevOps Enthusiast</br>
 Learning day by day</br>
 One Step ahead</br>
-Today I learned how to add reverse proxy and add domain to the app."""
+Today I learned how to add reverse proxy and add domain to the app
+And I add kubectl EKS in it."""
  
 @app.route("/health")
 def health():
