51eac951ebab354466a76e830719b49c3fa3bbc5
Uzair Munir <uzairchini555-gif>
2026-05-06 00:25:07 +0500

    Updated UI to professional dashbpard

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,6 @@
 FROM python:3.9
 WORKDIR /app
-# Copy dependency file first (best practise for caching)
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
-# Copy project files 
 COPY . .
-#CI/CD build metadata (will come from Github Actions)
-ARG VERSION 
-ARG COMMIT 
-ENV APP_VERSION=$VERSION 
-ENV APP_COMMIT=$COMMIT 
-HEALTHCHECK CMD curl --fail http://localhost:5000/ || exit 1
-EXPOSE 5000
+RUN pip install flask 
 CMD ["python", "app.py"]
 
--- app.py
+++ app.py
@@ -1,13 +1,65 @@
-from flask import Flask 
+from flask import Flask, render_template_string 
 app = Flask(__name__)
 @app.route("/")
 def home():
-	return """This is Uzair, DevOps Enthusiast</br>
-Learning day by day</br>
-One Step ahead</br>
-Today I learned how to add reverse proxy and add domain to the app</br>
-And I add kubectl EKS in it."""
- 
+	return render_template_string("""
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>DevOps CI/CD App</title>
+		<style>
+			 body {
+			    margin: 0;
+			    font-family: Arial, sans-serif;
+			    background: linear-gradient(135deg, #0f172a, #1e293b);
+		   	    color: white;
+			    text-align: center;
+			}
+			.container {
+	  		    padding-top: 120px;
+			}
+			h1 {
+			   font-size: 48px;
+			   margin-bottom: 10px;
+			}
+			.card {
+			   margin: 40px auto;
+			   width: 60%;
+			   padding: 20px;
+			   background: rgba(255,255,255,0.05);
+			   border-radius: 12px;
+			   box-shadow: 0 0 20px rgba(0,0,0,0.4);
+			}
+			.badge {
+			   display: inline-block;
+			   padding: 8px 14px;
+			   backgroud: #22c55e;
+			   border-radius: 20px;
+			   margin-top: 20px;
+			   font-size: 14px
+			}
+			footer {
+			   margin-top: 80px;
+			   font-size: 12px;
+			   color: #94a3b8;
+			}
+		</style>
+		
+	</head>
+	<body>
+		<div class="container">
+			<h1>🚀 DevOps CI/CD Pipeline</h1>
+			<p>Deployed on AWS EKS using Docker, Kubernetes & Github Actions</p>
+			<div class="card">
+				<h2>System Status</h2>
+				<p>Application is running successfully on Kubernetes cluster</p>
+				<div class="badge">✔ Deployment Active</div>
+			</div>
+			</footer>
+		</div>
+	</body>
+	</html>
+	""")
 @app.route("/health")
 def health():
 	return "OK", 200
